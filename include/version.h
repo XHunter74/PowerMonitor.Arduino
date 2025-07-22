83dbeec31772931a4639458fa552a7d8f93371fe
serhiy.krasovskyy <xhunter74@gmail.com>
2025-07-22 16:57:59 +0300

    Restored version.

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 
 #ifndef VERSION
-  #define VERSION "3.0.11"
+  #define VERSION "3.0.9"
 #endif
 #ifndef BUILD_DATE
-  #define BUILD_DATE "2025-07-22 13:37"
+  #define BUILD_DATE "2024-12-31 11:45"
 #endif
